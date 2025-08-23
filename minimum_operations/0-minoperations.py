2e684191bf06cc1b8aee720aa35ec26a39c9936f
Killian BEHAVA <behavakillian70@gmail.com>
2025-08-23 12:13:21 +0200

    Add comment (min. operations)

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+### MINIMUM OPERATIONS ALGORITHM ###
+"""
+
+
 def minOperations(n):
+    """
+    --- SECRET OF THE ALGORTIHM ---
+    Convert prime numbers multiplication to addition
+    1- Extract the prime numbers of n
+    2- Return the sum of them
+    """
     if not isinstance(n, int) or n < 2:
         return 0
 
