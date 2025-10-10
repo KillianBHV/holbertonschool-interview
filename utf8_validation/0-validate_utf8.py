8e23eb1fc66dde60a0b4e066ed07cbfb49608c01
Killian BEHAVA <behavakillian70@gmail.com>
2025-10-11 00:53:57 +0200

    UTF-8 Validator

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -20,21 +20,37 @@ def validUTF8(d):
         binary_value = bin(i).replace("0b", "")
         binary_value = binary_value.zfill(8*int(len(binary_value)/9)+8)
         binary_size = int(len(binary_value)/8)
-        
-        '''Bytes start and size check
-        ''' 
+
         if not 1 <= binary_size <= 4:
             return False
 
-        if binary_size == 2 and not binary_value[-8:].startswith('110'):
-            return False
-        elif binary_size == 3 and not binary_value[-8:].startswith('1110'):
-            return False
-        elif binary_size == 4 and not binary_value[-8:].startswith('11110'):
-            return False
-        elif binary_size == 1 and binary_value.startswith('10'):
-            return False
+        '''Bytes Check
+        '''
+        if offset == 0:
+            if binary_size == 2:
+                if not binary_value[-8:].startswith('110'):
+                    return False
+                offset = 1
+            elif binary_size == 3:
+                if not binary_value[-8:].startswith('1110'):
+                    return False
+                offset = 2
+            elif binary_size == 4:
+                if not binary_value[-8:].startswith('11110'):
+                    return False
+                offset = 3
+            else:
+                if binary_value.startswith('10'):
+                    return False
+        else:
+            if not binary_value[-8:].startswith('10'):
+                return False
+            offset -= 1
 
-    '''All characters are valid
     '''
+    If offset is not zero, there is an encoding error
+    Else all characters are valid
+    '''
+    if not offset == 0:
+        return False
     return True
