ff327b4c37a9dcd14ef42960b890b4c0c4fb7820
Killian BEHAVA <behavakillian70@gmail.com>
2025-10-10 23:34:44 +0200

    UTF-8 Validator

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -20,36 +20,20 @@ def validUTF8(d):
         binary_value = bin(i).replace("0b", "")
         binary_value = binary_value.zfill(8*int(len(binary_value)/9)+8)
         binary_size = int(len(binary_value)/8)
-
+        
         '''Bytes start and size check
-        '''
-        if binary_size == 1:
-            if not binary_value.startswith('0'):
-                return False
-        elif binary_size == 2:
-            if not binary_value.startswith('110'):
-                return False
-            else:
-                offset = 8
-        elif binary_size == 3:
-            if not binary_value.startswith('1110'):
-                return False
-            else:
-                offset = 16
-        elif binary_size == 4:
-            if not binary_value.startswith('11110'):
-                return False
-            else:
-                offset = 24
-        else:
+        ''' 
+        if not 1 <= binary_size <= 4:
             return False
 
-        '''All bytes except the MSB must starts with 10 in binary
-        '''
-        while offset > 0:
-            if not binary_value[offset:].startswith('10'):
-                return False
-            offset -= 8
+        if binary_size == 2 and not binary_value[-8:].startswith('110'):
+            return False
+        elif binary_size == 3 and not binary_value[-8:].startswith('1110'):
+            return False
+        elif binary_size == 4 and not binary_value[-8:].startswith('11110'):
+            return False
+        elif binary_value == 1 and binary_value.startswith('10'):
+            return False
 
     '''All characters are valid
     '''
