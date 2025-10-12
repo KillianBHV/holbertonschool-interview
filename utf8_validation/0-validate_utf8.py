016af9a4c455e0ea2ac2492769360f2b9545f3a2
Killian BEHAVA <behavakillian70@gmail.com>
2025-10-12 08:46:14 +0200

    UTF-8 Validator

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -17,33 +17,21 @@ def validUTF8(d):
     for i in d:
         '''Integer to string conversion
         '''
-        binary_value = bin(i).replace("0b", "")
-        binary_value = binary_value.zfill(8*int(len(binary_value)/9)+8)
-        binary_size = int(len(binary_value)/8)
-
-        if not 1 <= binary_size <= 4:
-            return False
+        binary_value = bin(i).replace("0b", "").zfill(8)[-8:]
 
         '''Bytes Check
         '''
         if offset == 0:
-            if binary_size == 2:
-                if not binary_value[-8:].startswith('110'):
-                    return False
+            if binary_value.startswith('110'):
                 offset = 1
-            elif binary_size == 3:
-                if not binary_value[-8:].startswith('1110'):
-                    return False
+            elif binary_value.startswith('1110'):
                 offset = 2
-            elif binary_size == 4:
-                if not binary_value[-8:].startswith('11110'):
-                    return False
+            elif binary_value.startswith('11110'):
                 offset = 3
-            else:
-                if binary_value.startswith('10'):
-                    return False
+            elif binary_value.startswith('10'):
+                return False
         else:
-            if not binary_value[-8:].startswith('10'):
+            if not binary_value.startswith('10'):
                 return False
             offset -= 1
 
