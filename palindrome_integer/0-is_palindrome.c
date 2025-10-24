cd583063f2aae9e927b630a8fbd46424b2c6a86c
Killian BEHAVA <behavakillian70@gmail.com>
2025-10-24 10:21:31 +0200

    Palindrome Integer

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "palindrome.h"
+/**
+ * is_palindrome - checks if a number is a palindrome
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 if it is a palindrome, 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long modulo_ref = n;
+	unsigned long modulo_iteratif = 0;
+
+	while (modulo_ref != 0)
+	{
+		modulo_iteratif *= 10;
+		modulo_iteratif += modulo_ref % 10;
+		modulo_ref /= 10;
+	}
+
+	if (modulo_ref == modulo_iteratif)
+		return (1);
+	return (0);
+}
+
---
+++ palindrome_integer/palindrome.h
@@ -0,0 +1,10 @@
+#ifndef _PALINDROME_INT_H
+#define _PALINDROME_INT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int is_palindrome(unsigned long n);
+
+#endif
+
