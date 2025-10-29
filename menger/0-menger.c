0ae95c58b16972a6714b97a14b088e876d9fda41
Killian BEHAVA <11841@holbertonstudents.com>
2025-10-29 11:37:10 +0100

    Menger Sponge Algorithm

---
+++ menger/0-menger.c
@@ -0,0 +1,29 @@
+#include "menger.h"
+
+void menger(int level)
+{
+	int x, y, n, m, i;
+	char c;
+
+	m = pow(3, level);
+	for (n = 0; n < m; n++)
+	{
+		for (i = 0; i < m;)
+		{
+			c = '#';
+			x = n;
+			y = i++;
+
+			while (y > 0 || x > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+					c = ' ';
+				x /= 3;
+				y /= 3;
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+	}
+}
+
---
+++ menger/menger.h
@@ -0,0 +1,11 @@
+#ifndef _MENGER_H
+#define _MENGER_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void menger(int level);
+
+#endif
+
