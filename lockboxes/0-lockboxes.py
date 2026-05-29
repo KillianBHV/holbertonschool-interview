ed7ba055078bc5661ccc2891235145aed7bff82c
KillianBHV <11841@holbertonstudents.com>
2026-05-29 04:39:56 -0400

    Lockboxes - pycodestyle validation: OK

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Description:
+        Determines if all boxes can be opened
+
+    Params:
+        boxes: all boxes with their keys
+
+    Returns:
+        True or False
+    """
+    # ENTRY POINT TYPE CHECK
+    # Returns immediately False if not satisfied
+    if not (
+        isinstance(boxes, list)
+        and all(isinstance(x, list) for x in boxes)
+    ):
+        return False
+
+    # CASE 1: There is no box
+    # CASE 2: The is one box only
+    number_of_boxes = len(boxes)
+    if number_of_boxes == 1:
+        return True
+
+    # MASTER CASE
+    # Key Initialisation
+    max_value = 0
+
+    for box in boxes:
+        for val in box:
+            if val > max_value:
+                max_value = val
+
+    keys = []
+    for i in range(number_of_boxes):
+        keys.append(False)
+
+    # First Box Keys
+    for key in boxes[0]:
+        keys[0] = True
+
+    # Other Boxes
+    # PS - WARNING: First loop is non-removable
+    for i in range(number_of_boxes):
+        for box in range(number_of_boxes):
+            for key in boxes[box]:
+                if keys[box] and key < number_of_boxes:
+                    keys[key] = True
+
+        if all(keys):
+            return True
+
+    return all(keys)
