68fa1dab3325575dcb180c82793f423a9777dbcd
KillianBHV <11841@holbertonstudents.com>
2026-06-18 16:14:49 +0200

    Test Optimization - 1

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -2,42 +2,73 @@
 #include "lists.h"
 
 /**
+ * update_half_of_list - Get the first value of a second half listint_t list
+ * @head: points to the first node of a listint_t list
+ * Return: 1 if it is a palindrome, otherwise 0.
+ */
+listint_t *update_half_of_list(listint_t **head, const int hf_nb_nodes)
+{
+	listint_t *start;
+	listint_t *current = NULL;
+	int n;
+
+	start = *head;
+
+	for (int i = 0; i < hf_nb_nodes; i++)
+	{
+		current = start;
+		
+		for (int j = i; j < hf_nb_nodes; j++)
+		{
+			n = current->n;
+			current->n = start->n;
+			start->n = n;
+			current = current->next;
+		}
+		start = start->next;
+	}
+
+	return current;
+}
+
+/**
  * is_palindrome - Checks if list chains a palindrome
  * @head: points to the first node of a listint_t list
  * Return: 1 if it is a palindrome, otherwise 0.
  */
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
+	
+	listint_t *start = *head;
+	listint_t *current = NULL;
+	int node_index = 1;
+	int is_odd = 0;
 
-	listint_t *left;
-	listint_t *right;
-	unsigned int n;
-
-	n = 0;
-	left = *head;
-	while (left->next != NULL)
+	while (start->next != NULL)
 	{
-		n++;
-		left = left->next;
+		start = start->next;
+		node_index++;
 	}
 
-	for (int i = n; i >= 0; i--)
+	start = *head;
+	if (node_index % 2 == 1)
+		is_odd = 1;
+
+	node_index /= 2;
+	current = update_half_of_list(head, node_index);
+
+	if (is_odd)
+		current = current->next;
+
+	while (current->next != NULL)
 	{
-		left = *head;
-		right = *head;
-
-		for (int j = (n - i); j > 0; j--)
-			left = left->next;
-		for (int j = i; j > 0; j--)
-			right = right->next;
-
-		if (left->n != right->n)
+		if (start->n != current->n)
 			return (0);
 
-		if (left->next == right || left->next == right->next)
-			break;
+		start = start->next;
+		current = current->next;
 	}
 
 	return (1);
