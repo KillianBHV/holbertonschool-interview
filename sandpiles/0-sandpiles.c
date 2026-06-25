15935c8e1a68ede714fba22412cbd7365c865040
KillianBHV <11841@holbertonstudents.com>
2026-06-25 16:22:45 +0200

    Update 0-sandpiles.c

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -46,7 +46,7 @@ int is_stabilized_sandpile(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, base;
+	int i, j;
 
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
