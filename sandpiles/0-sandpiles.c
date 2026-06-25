cb0f60d43305af1ca4ae134132bd06d96a2792c9
KillianBHV <11841@holbertonstudents.com>
2026-06-25 15:48:19 +0200

    Update

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -3,30 +3,6 @@
 #include "sandpiles.h"
 
 /**
- * print_grid_sum - Print 3x3 grids sum
- * @grid1: Left 3x3 grid
- * @grid2: Right 3x3 grid
- */
-static void print_grid_sum(int grid1[3][3], int grid2[3][3])
-{
-	int i, j;
-
-	i = grid1[0][0];
-	i++;
-
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if (j)
-				printf(" ");
-			printf("%d", grid2[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-/**
  * print_grid - Print 3x3 grid
  * @grid: 3x3 grid
  */
@@ -71,8 +47,6 @@ int is_stabilized_sandpile(int grid[3][3])
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
-
-	print_grid_sum(grid1, grid2);
 
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
