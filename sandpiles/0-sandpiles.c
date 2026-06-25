ac3d8e97a1c38febdcb59d59d4aa6016a5f61b78
KillianBHV <11841@holbertonstudents.com>
2026-06-25 15:45:50 +0200

    Update

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -11,17 +11,11 @@ static void print_grid_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
 
-	/* for (i = 0; i < 3; i++)
+	i = grid1[0][0];
+	i++;
+
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 3; j++)
-		{
-			if (j)
-				printf(" ");
-			printf("%d", grid1[i][j]);
-		}
-
-		printf(" %c ", (i == 1 ? '+' : ' ')); */
-
 		for (j = 0; j < 3; j++)
 		{
 			if (j)
