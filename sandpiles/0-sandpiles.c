10d526d90472e6dd823dd5d52b6a914f90e4404a
KillianBHV <11841@holbertonstudents.com>
2026-06-25 16:25:37 +0200

    Update 0-sandpiles.c

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -52,8 +52,8 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		for (int j = 0; j < 3; j++)
 			grid1[i][j] += grid2[i][j];
 	
-	printf("=\n");
-	print_grid(grid1);
+	/* printf("=\n"); */
+	/* print_grid(grid1); */
 	while (!is_stabilized_sandpile(grid1))
 	{
 		for (i = 0; i < 3; i++)
